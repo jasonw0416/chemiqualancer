729fb1b095e11494cb5ff8356e91ad465d7da65b
Wonjun Lee <jasonw0416@gmail.com>
2021-01-20 01:11:12 +0900

    display result

--- matrix_processor.js
+++ matrix_processor.js
@@ -1,46 +1,49 @@
-var matrix = [[ 1, 0, -2, 0 ], [ 1, 4, -4, -1 ], [ 1, 2, 0, -2 ], [ 0, 1, -1, 0 ]];
+var matrix = [[ 1, 0, -2, 0, 0 ], [ 1, 4, -4, -1, 0 ], [ 1, 2, 0, -2, 0 ], [ 0, 1, -1, 0, 0 ]];
 
-console.log(process_matrix_to_coefficients(4, 4));
 
-function rref(rows, colummns) {
+console.log(process_matrix_to_coefficients(4, 5));
+
+
+
+
+function rref(rows, columns) {
     var pivot = 0;
     for (r = 0; r < rows; ++r) {
-        if (columns <= pivot) {
-            console.log("Matrix is not solvable");
-            return 0;
-        }
 
         var i = r;
         while (matrix[i][pivot] === 0) {
+            if (columns <= pivot)
+                return;
             ++i;
             if (rows === i) {
                 i = r;
                 ++pivot;
-                if (colummns === pivot) {
-                    console.log("Matrix is not solvable");
-                    return 0;
-                }
-            }
-            //swapping rows
-            if (i !== r) {
-                for (j = 0; j < colummns; ++j) {
-                    swap(i, j, r);//swaps rows
-                }
-            }
+                if (columns === pivot)
+                    return;
 
-            var val = matrix[r][pivot];
-            for (j = 0; j < colummns; ++j)
-                matrix[r][j] /= val;
-
-            for (i = 0; i < rows; ++i) {
-                if (i !== r) {
-                    val = matrix[i][pivot];
-                    for (j = 0; j < colummns; ++j)
-                        matrix[i][j] -= val * matrix[r][j];
-                }
             }
-            pivot++;
         }
+        for (k = 0; k < columns; ++k) {
+            var temp = matrix[i][k];
+            matrix[i][k] = matrix[r][k];
+            matrix[r][k] = temp;
+        }
+
+        var val = matrix[r][pivot];
+
+        for (j = 0; j < columns; ++j){
+            matrix[r][j] /= val;
+        }
+
+        for (i = 0; i < rows; ++i) {
+            if (i !== r) {
+                val = matrix[i][pivot];
+                for (j = 0; j < columns; ++j){
+                    matrix[i][j] -= val * matrix[r][j];
+                }
+            }
+        }
+        pivot++;
     }
 }
 
@@ -87,17 +90,10 @@ function reformat_matrix(rows, columns){ //formats -0 to 0
 }
 
 function gcd(a, b){ //finds gcd via Euclidean Algorithm
-    if (a === 0){
-        return b;
-    }
-    else if (b === 0){
+    if (!b) {
         return a;
     }
-
-    if (a < b){
-        return gcd(a, b % a);
-    }
-    else{
+    else {
         return gcd(b, a % b);
     }
 }
@@ -110,7 +106,7 @@ function decimal_to_fraction(decimal){
 
     var big_gcd = gcd(Math.round(fractional_part * precision), precision);
 
-    var denominator = precision / gcd_;
+    var denominator = precision / big_gcd;
     var numerator = (Math.round(fractional_part * precision) / big_gcd) + (integral_part * denominator);
     return [numerator, denominator];
 }
@@ -121,20 +117,17 @@ function lcm(a, b) {//a * b = lcm(a, b) * gcd (a, b)
 
 function find_free_variables(rows, columns){//creates a boolean list that states if a variable is a free variable 0 is false 1 is true
     var free_variable_list = [];
-    var is_all_zero = 1;
+    for (j = 0; j < matrix.length; ++j){
+        free_variable_list.push(0);
+    }
     for (i = 0; i < rows; ++i){
         for(j = 0; j < columns; ++j){
             if(matrix[i][j] !== 0){
-                is_all_zero = 0;
                 break;
             }
-            if(is_all_zero === 1 && j === columns - 1){
-                free_variable_list.push(1);
+            if(j === columns - 1){
+                free_variable_list[i] = 1;
             }
-            else if(is_all_zero === 0 && j === columns - 1){
-                free_variable_list.push(0);
-            }
-
         }
     }
     return free_variable_list;
@@ -145,12 +138,15 @@ function find_convient_lcm_for_free_variables(rows, columns){
     var list_of_numbers = [];
     for (i = 0; i < rows; ++i){
         for (j = 0; j < columns; ++j){
-            list_of_numbers.push(decimal_to_fraction(matrix[i][j])); //converts to a pair (first = numerator, second = denominator)
+            if(matrix[i][j] === 0){
+                continue;
+            }
+            list_of_numbers.push(matrix[i][j]);
         }
     }
     var least_common_denomiator = 1;
-    for (k = 0; k < list_of_numbers.length; ++l){
-        least_common_denomiator= lcm(list_of_numbers[k][1], least_common_denomiator); //finds lcm of all numbers in list
+    for (k = 0; k < list_of_numbers.length; ++k){
+        least_common_denomiator= lcm(list_of_numbers[k], least_common_denomiator); //finds lcm of all numbers in list
     }
     return least_common_denomiator;
 }
@@ -163,7 +159,13 @@ function convert_matrix_to_integers(rows, columns){
         }
     }
 
-    var least_common_denomiator = find_convient_lcm_for_free_variables(rows, columns);
+    var least_common_denomiator = 1;
+    for (k = 0; k < list_of_numbers.length; ++k){
+        if(list_of_numbers[k][1] === 0){
+            continue;
+        }
+        least_common_denomiator= lcm(list_of_numbers[k][1], least_common_denomiator); //finds lcm of all numbers in list
+    }
 
     for(l = 0; l < list_of_numbers.length; ++l){
         list_of_numbers[l] = (least_common_denomiator / list_of_numbers[l][1]) * list_of_numbers[l][0]; //converts all numbers in list to integers using lcm
@@ -184,11 +186,10 @@ function process_matrix_to_coefficients(rows, columns) {//converts the matrix to
     }
 
     var free_variables = [];
-    ref(rows, columns);
+    rref(rows, columns);
     reformat_matrix(rows, columns);
-    console.log(matrix);
+    convert_matrix_to_integers(rows, columns);
     free_variables = find_free_variables(rows, columns);
-    console.log(free_variables);
     for (i = 0; i < free_variables.length; ++i){
         if(free_variables[i]){
             coefficients[i] = find_convient_lcm_for_free_variables(rows, columns);
@@ -197,5 +198,9 @@ function process_matrix_to_coefficients(rows, columns) {//converts the matrix to
             coefficients[i] = matrix[i][columns - 2];
         }
     }
+    for (j = 0; j < coefficients.length; ++j){
+        coefficients[j] = Math.abs(coefficients[j]);
+    }
+
     return coefficients;
 }
