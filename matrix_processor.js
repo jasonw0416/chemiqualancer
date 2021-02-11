3b3ef1f71451256de661fc8f3ad7fb1054b05bc9
David Du <davdu21@bergen.org>
2021-02-10 21:48:01 -0500

    fixed free variable finder

--- matrix_processor.js
+++ matrix_processor.js
@@ -110,15 +110,24 @@ function lcm(a, b) {//a * b = lcm(a, b) * gcd (a, b)
 function find_free_variables(rows, columns){//creates a boolean list that states if a variable is a free variable 0 is false 1 is true
     var free_variable_list = [];
     for (j = 0; j < matrix.length; ++j){
-        free_variable_list.push(0);
+        free_variable_list.push(1);
     }
-    for (i = 0; i < rows; ++i){
-        for(j = 0; j < columns; ++j){
-            if(matrix[i][j] !== 0){
-                break;
-            }
-            if(j === columns - 1){
-                free_variable_list[i] = 1;
+    if(matrix[0][0] === 1){
+        free_variable_list[0] = 0;
+    }
+    for (i = 1; i < columns; ++i){
+        for(j = 0; j < rows; ++j){
+            if(matrix[j][i] === 1){
+                var isPivot = 1;
+                for (z = i - 1; z >= 0; z--){
+                    if(matrix[j][z] !== 0){
+                        isPivot = 0;
+                    }
+                }
+                if(isPivot === 1){
+                    free_variable_list[i] = 0;
+                    break;
+                }
             }
         }
     }
@@ -207,6 +216,15 @@ function fractioniseMatrix(){
     }
 }
 
+function printMatrix(){
+    for (i = 0; i < matrix.length; i++){
+        for (j = 0; j < matrix[0].length; j++){
+            console.log(matrix[i][j] + " ");
+        }
+        console.log("\n");
+    }
+}
+
 function process_matrix_to_coefficients(rows, columns) {//converts the matrix to a list of coefficients corresponding with the order of compounds as they appear.
     var coefficients = [];
     for (j = 0; j < matrix[0].length - 1; ++j){
@@ -219,7 +237,9 @@ function process_matrix_to_coefficients(rows, columns) {//converts the matrix to
     rref(rows, columns);
     reformat_matrix(rows, columns);
     convert_matrix_to_integers(rows, columns);
+    printMatrix();
     free_variables = find_free_variables(rows, columns);
+    console.log(free_variables);
     for (i = 0; i < free_variables.length; ++i){
         if(free_variables[i]){
             coefficients[i] = find_convient_lcm_for_free_variables(rows, columns);
